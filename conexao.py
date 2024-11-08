4ea06810ef9dc054d4fcd2786e1da89e09f0bbfc
Maria Thome <maria.thome4405@gmail.com>
2024-11-08 10:08:04 -0300

    .

--- conexao.py
+++ conexao.py
@@ -4,21 +4,21 @@ class Conexao:
     def conectar():
         # chama a função connect do módulo mysql.connector para estabelecer uma nova conexão com o banco de dados MySQL
         mydb = mysql.connector.connect(
-            host ="logclassbase.mysql.database.azure.com",
+            host ="127.0.0.1",
             # não esquecer de modificar isso conforme a porta do banco de dados
-            user ="logclassadm",
-            password ="TccSenai.2024",
-            database ="databaseprofessor"
+            user ="usuario_logclass",
+            password ="logclass",
+            database ="databaseProfessor"
         )
 
         return mydb
     # função que irá executar um determinado banco de dados (o qual será escolhido pelo aluno)
     def conectarAluno(databaseAluno):
         mydb = mysql.connector.connect(
-            host ="logclassbase.mysql.database.azure.com",
+            host ="127.0.0.1",
             # não esquecer de modificar isso conforme a porta do banco de dados
-            user ="logclassadm",
-            password ="TccSenai.2024",
+            user ="usuario_logclass",
+            password ="logclass",
             database = databaseAluno           
         )
 
